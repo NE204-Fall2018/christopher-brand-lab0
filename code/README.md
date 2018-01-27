f1e90a6ec2327bbf10cd5eb21f48e873623d4c1c
Matthew Marshall <Matt@Matthews-MacBook-Air-2.local>
2018-01-27 15:42:07 -0800

    Updated the README.md file for the code directory. Cleaned up the file and added some comments

---
+++ code/README.md
@@ -0,0 +1,21 @@
+# NE 204 Report Lab0 Code Section
+
+# Energy Calibration
+
+## calibration.py
+
+calibration.py is a function that takes a user input of isotopes
+and returns the corresponding gamma-ray energies. An example input is shown. The isotopes need to be separated by a comma
+
+```
+energy_calibration('Cs137', 'Am241', 'Co60')
+
+```
+
+### calibration_lab0.py
+
+This code is almost fully automated to run. Enter the gamma-ray energies you
+are interested in calibrating your spectrum with by updating
+energy_calibration within the code.
+The width of the peaks to remove might need to be adjusted as well depending
+on the peak sizes. 
--- code/calibration_lab0.py
+++ code/calibration_lab0.py
@@ -1,12 +1,28 @@
 '''
 fit_analysis.py
 fit_analysis.py analyzes a spectrum and creates an energy calibration.
+It uses the function energy_calibration to calibrate an energy spectrum.
+Two points are needed for this linear calibration.
 '''
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from modelling import gauss
 import statsmodels.api as sm
+
+'''
+Enter in your calibration sources into energy_calibration
+energy_calibration('Co60', 'Cs137')
+'''
+from calibration import energy_calibration
+energy_list = energy_calibration('Cs137', 'Am241')
+energy_list = sorted(energy_list, key=int)
+
+channel_width = 10
+
+'''
+The file is generated from the make file.
+'''
 
 fname = '../lab0_spectral_data.txt'
 with open(fname, 'r') as f:
@@ -22,22 +38,34 @@ Cs137 = data[:,2]
 Co60 = data[:,3]
 Eu152 = data[:,4]
 
-from calibration import energy_calibration
-energy_list = energy_calibration('Cs137', 'Am241')
-energy_list = sorted(energy_list, key=int)
-
+'''
+merging the data for the calibration
+Also converting merged data into a list so channels can be
+removed easier.
+'''
 merged_data = data[:,0] + data[:,2]
 merged_data = np.array(merged_data).tolist()
+plt.plot(merged_data)
 
+'''
+The while loop goes through and identifies the largest peak in the
+spectrum and it records the position of that peak. It then removes
+the peak by removing 10 channels from the right and left of the peak.
+The code will then search for the next largest position.
+'''
 i = 0; channel_max_list = []
 while i < len(energy_list):
     channel_max = np.argmax(merged_data)
-    data_left = channel_max - 10
-    data_right = channel_max + 10
+    data_left = channel_max - channel_width
+    data_right = channel_max + channel_width
     del merged_data[data_left:data_right]
     channel_max_list.append(channel_max)
     i += 1
 
+'''
+sorting channel number so the correct channel number corresponds with
+the correct energy.
+'''
 channel_number = sorted(channel_max_list, key=int)
 energy = energy_list
 results = sm.OLS(energy,sm.add_constant(channel_number)).fit()
