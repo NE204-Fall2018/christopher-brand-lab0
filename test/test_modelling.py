ad1ad5664f07d75dc71d60087b5168670ee0d9c2
Christopher Brand <christopher.brand@berkeley.edu>
2018-09-13 15:45:32 -0700

    working test

--- test/test_modelling.py
+++ test/test_modelling.py
@@ -3,7 +3,8 @@ sys.path.append('../')
 
 from chrisBrand_lab0.code.modelling import gauss
 from nose.tools import assert_almost_equal
-from chrisBrand_lab0.code.calibration_lab0 import spectrum_calibration
+#from chrisBrand_lab0.code.calibration_lab0 import spectrum_calibration
+#from chrisBrand_lab0.code.gamma_energies import gamma_energies
 import pytest
 
 def test_gauss_symmetry():
@@ -12,6 +13,6 @@ def test_gauss_symmetry():
     C = 1
     xr = 2.0
     xl = -xr
-#    assert gauss(xr, A, B, C) == gauss(xl, A,B,C)
-    assert_almost_equal(xr, A, B, C)
-    slope, intercept = spectrum_calibration(1, 2, np.array(range(4)))
+    assert_almost_equal(gauss(xr,A,B,C), gauss(xl,A,B,C))
+#    assert_almost_equal(xr, A, B, C)
+#    slope, intercept = spectrum_calibration(1, 2, np.array(range(4)))
